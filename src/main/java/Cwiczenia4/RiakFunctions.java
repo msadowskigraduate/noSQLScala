3fab303f002b3e1dce2656bee67bcfce52808808
Michal Sadowski <michal.sadowski@roche.com>
2018-06-17 12:47:51 +0200

    cleanup

--- src/main/java/Cwiczenia4/RiakFunctions.java
+++ src/main/java/Cwiczenia4/RiakFunctions.java
@@ -1,9 +1,13 @@
 package Cwiczenia4;
 
 import Cwiczenia4.domain.Person;
+import Cwiczenia4.domain.PersonUpdate;
 import com.basho.riak.client.api.RiakClient;
-import com.basho.riak.client.api.cap.UnresolvedConflictException;
+import com.basho.riak.client.api.commands.kv.DeleteValue;
 import com.basho.riak.client.api.commands.kv.FetchValue;
+import com.basho.riak.client.api.commands.kv.StoreValue;
+import com.basho.riak.client.api.commands.kv.UpdateValue;
+import com.basho.riak.client.core.RiakFuture;
 import com.basho.riak.client.core.query.Location;
 import com.basho.riak.client.core.query.Namespace;
 
@@ -14,9 +18,7 @@ import java.util.concurrent.ExecutionException;
  */
 public class RiakFunctions {
 
-    public static Location generateLocationForKey(String key) {
-        return new Location(new Namespace("s17983"), key);
-    }
+    public static String BUCKET_NAME = "s17983";
 
     public static Person getValueForKey(RiakClient client, Location location) throws ExecutionException, InterruptedException {
         FetchValue fv = new FetchValue.Builder(location).build();
@@ -24,5 +26,33 @@ public class RiakFunctions {
         return response.getValue(Person.class);
     }
 
-    
+    public static String deleteEntryForKey(RiakClient client, Location key) {
+        DeleteValue dv = new DeleteValue.Builder(key).build();
+        RiakFuture response = client.executeAsync(dv);
+        return response.toString();
+    }
+
+    public static String addNewEntryWithKey(RiakClient client, Location key, Object itemToStore) throws ExecutionException, InterruptedException {
+        StoreValue.Response svResponse;
+        if(key!= null) {
+            StoreValue sv = new StoreValue.Builder(itemToStore).withLocation(key).build();
+            svResponse = client.execute(sv);
+            return svResponse.toString();
+        }
+        return null;
+    }
+
+    public static String modifyEntryByKey(RiakClient client, Location key, PersonUpdate update) throws ExecutionException, InterruptedException {
+        UpdateValue updateValue = new UpdateValue.Builder(key).withUpdate(update).build();
+        UpdateValue.Response response = client.execute(updateValue);
+        return response.toString();
+    }
+
+    public static Location generateLocationForKey(String key) {
+        return new Location(new Namespace(BUCKET_NAME), key);
+    }
+
+    public static void performPreRunCleanup(RiakClient client) {
+        deleteEntryForKey(client, generateLocationForKey("testPerson"));
+    }
 }
